e2bef04b9fca11df53069f2995fb4f468b43fc49
Vivek <nksaivivek@gmail.com>
2024-12-26 23:51:32 +0530

    Projects

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,14 +1,15 @@
 "use client";
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
+import { Spotlight } from "../../components/ui/Spotlight";
 
 const About = () => {
   const router = useRouter();
 
   return (
-    <div className="p-6 min-h-screen">
+    <div className="h-[40rem] w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
       <button
         className="absolute top-5 left-5 p-2 bg-gray-700 text-white rounded-full hover:bg-gray-600"
-        onClick={() => router.push('/')}
+        onClick={() => router.push("/")}
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -25,10 +26,20 @@ const About = () => {
           />
         </svg>
       </button>
-
-      <div className="text-center">
-        <h1 className="text-3xl font-bold">About Me</h1>
-        <p className="mt-4">I am a tech enthusiast...</p>
+      <Spotlight
+        className="-top-40 left-0 md:left-60 md:-top-20"
+        fill="white"
+      />
+      <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
+        <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+          Spotlight <br /> is the new trend.
+        </h1>
+        <p className="mt-4 font-playfair text-xl text-neutral-300 max-w-xl text-center mx-auto">
+          Spotlight effect is a great way to draw attention to a specific part
+          of the page. Here, we are drawing the attention towards the text
+          section of the page. I don&apos;t know why but I&apos;m running out of
+          copy.
+        </p>
       </div>
     </div>
   );
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -6,6 +6,10 @@ import React from "react";
 import { CardBody, CardContainer, CardItem } from "../../components/ui/3d-card";
 import Link from "next/link";
 import rollsroyce from "../../components/assets/rollsroyce.jpg";
+import MTJ from '../../components/assets/MTJ.png';
+import myportfolio from '../../components/assets/myportfolio.png';
+import stockastix from '../../components/assets/stockastix.png';
+import mymoney from '../../components/assets/mymoney.png';
 
 const Projects = () => {
   const router = useRouter();
@@ -14,7 +18,7 @@ const Projects = () => {
     <div className="p-6 min-h-screen relative">
       <button
         className="absolute top-4 left-4 p-2 bg-gray-700 text-white rounded-full hover:bg-gray-600"
-        onClick={() => router.push('/')}
+        onClick={() => router.push("/")}
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -32,52 +36,161 @@ const Projects = () => {
         </svg>
       </button>
 
-      <div className="text-center"><h1 className="text-3xl font-bold">Projects</h1>
-      <p className="mt-4">Here are some of my projects...</p></div>
-      <CardContainer className="inter-var">
-        <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border">
-          <CardItem
-            translateZ="50"
-            className="text-xl font-bold text-neutral-600 dark:text-white"
-          >
-            Make things float in air
-          </CardItem>
-          <CardItem
-            as="p"
-            translateZ="60"
-            className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
-          >
-            Hover over this card to unleash the power of CSS perspective
-          </CardItem>
-          <CardItem translateZ="100" className="w-full mt-4">
-            <Image
-              src={rollsroyce}
-              height="1000"
-              width="1000"
-              className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
-              alt="thumbnail"
-            />
-          </CardItem>
-          <div className="flex justify-between items-center mt-20">
+      <div className="text-center font-playfair">
+        <h1 className="text-3xl">Projects</h1>
+        <p className="mt-4 text-lg">Here are some of my projects...</p>
+      </div>
+      <div className="flex justify-evenly">
+        <CardContainer className="inter-var">
+          <CardBody className="bg-gray-50 w-[600px] relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] h-auto rounded-xl p-6 border">
             <CardItem
-              translateZ={20}
-              as={Link}
-              href="https://twitter.com/mannupaaji"
-              target="__blank"
-              className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+              translateZ="50"
+              className="text-xl font-bold text-neutral-600 dark:text-white"
             >
-              Try now →
+              My Trade Journal
             </CardItem>
+            <CardItem translateZ="100" className="w-full mt-4">
+              <Image
+                src={MTJ}
+                height="1000"
+                width="1000"
+                className="h-60 w-full object-cover rounded-lg group-hover/card:shadow-xl"
+                alt="thumbnail"
+              />
+            </CardItem>
+            <div className="flex justify-between items-center mt-5">
+              <CardItem
+                translateZ={20}
+                as={Link}
+                href="https://mytradejournal.in/"
+                target="__blank"
+                className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+              >
+                Try now →
+              </CardItem>
+              <CardItem
+                translateZ={20}
+                as="button"
+                className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+              >
+                Know more...
+              </CardItem>
+            </div>
+          </CardBody>
+        </CardContainer>
+        <CardContainer className="inter-var">
+          <CardBody className="bg-gray-50 w-[600px] relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] h-auto rounded-xl p-6 border">
             <CardItem
-              translateZ={20}
-              as="button"
-              className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+              translateZ="50"
+              className="text-xl font-bold text-neutral-600 dark:text-white"
             >
-              Sign up
+              Stockastix
             </CardItem>
-          </div>
-        </CardBody>
-      </CardContainer>
+            <CardItem translateZ="100" className="w-full mt-4">
+              <Image
+                src={stockastix}
+                height="1000"
+                width="1000"
+                className="h-60 w-full object-cover rounded-lg group-hover/card:shadow-xl"
+                alt="thumbnail"
+              />
+            </CardItem>
+            <div className="flex justify-between items-center mt-5">
+              <CardItem
+                translateZ={20}
+                as={Link}
+                href="https://twitter.com/mannupaaji"
+                target="__blank"
+                className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+              >
+                Demo →
+              </CardItem>
+              <CardItem
+                translateZ={20}
+                as="button"
+                className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+              >
+                Know more...
+              </CardItem>
+            </div>
+          </CardBody>
+        </CardContainer>
+      </div>
+      <div className="flex justify-evenly">
+        <CardContainer className="inter-var">
+          <CardBody className="bg-gray-50 w-[600px] relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] h-auto rounded-xl p-6 border">
+            <CardItem
+              translateZ="50"
+              className="text-xl font-bold text-neutral-600 dark:text-white"
+            >
+              My Money
+            </CardItem>
+            <CardItem translateZ="100" className="w-full mt-4">
+              <Image
+                src={mymoney}
+                height="1000"
+                width="1000"
+                className="h-60 w-full object-cover rounded-lg group-hover/card:shadow-xl"
+                alt="thumbnail"
+              />
+            </CardItem>
+            <div className="flex justify-between items-center mt-5">
+              <CardItem
+                translateZ={20}
+                as={Link}
+                href="https://twitter.com/mannupaaji"
+                target="__blank"
+                className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+              >
+                Demo →
+              </CardItem>
+              <CardItem
+                translateZ={20}
+                as="button"
+                className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+              >
+                Know more...
+              </CardItem>
+            </div>
+          </CardBody>
+        </CardContainer>
+        <CardContainer className="inter-var">
+          <CardBody className="bg-gray-50 w-[600px] relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] h-auto rounded-xl p-6 border">
+            <CardItem
+              translateZ="50"
+              className="text-xl font-bold text-neutral-600 dark:text-white"
+            >
+              Portfolio
+            </CardItem>
+            <CardItem translateZ="100" className="w-full mt-4">
+              <Image
+                src={myportfolio}
+                height="1000"
+                width="1000"
+                className="h-60 w-full object-cover rounded-lg group-hover/card:shadow-xl"
+                alt="thumbnail"
+              />
+            </CardItem>
+            <div className="flex justify-between items-center mt-5">
+              <CardItem
+                translateZ={20}
+                as={Link}
+                href="/"
+                className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+              >
+                Try now →<br/>(Do not click...you might be redirected to the beginning page)
+              </CardItem>
+              <CardItem
+                translateZ={20}
+                as="button"
+                className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
+              >
+                Know more...
+              </CardItem>
+            </div>
+          </CardBody>
+        </CardContainer>
+      </div>
     </div>
   );
 };
